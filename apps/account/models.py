0b6bb413b978eb7de693703ff30bbe99dd3283e6
RinatNaipbekov <nasipbekovrinat@gamil.com>
2022-10-17 20:03:24 +0600

    first_commit

---
+++ apps/account/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def _create(self, username, email, password,  **extra_fields):
+        if not username:
+            raise ValueError('User must have username')
+        if not email:
+            raise ValueError('User must have email')
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            **extra_fields
+        )   
+        user.set_password(password)
+        user.save(usign=self._db) 
+        return user
+
+    def create_user(self, username, email, password, **extra_fields ):
+        extra_fields.setdefault('is_stuff', False)
+        extra_fields.setdefault('is_active', False)
+        return self._create(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email, password, **extra_fields ):
+        extra_fields.setdefault('is_stuff', True)
+        extra_fields.setdefault('is_active', True)
+        return self._create(username, email, password, **extra_fields)            
+
+
+
+class User(AbstractBaseUser):
+    username = models.CharField('Username', max_length=50, primary_key=True)
+    email = models.EmailField('Email', max_length=255, primary_key=True)
+    is_active = models.BooleanField(default=False)
+    is_stuff = models.BooleanField(default=False)
+    activation_code = models.CharField(max_length=8, blank=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self) -> str:
+        return self.username
+
+
+    def has_module_perms(self, app_label):
+        return self.is_stuff
+
+    def has_perm(self, obj=None):
+        return self.is_stuff    
